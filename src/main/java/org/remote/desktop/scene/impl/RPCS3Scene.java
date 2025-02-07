6932cb14411857c57c971ab2eb39fb9ce88ce9c6
Marek Manco <marek.manco@schlieger.cz>
2025-02-07 22:42:55 +0100

    add fullscreen and timeout on rpcs3 scene;

--- src/main/java/org/remote/desktop/scene/impl/RPCS3Scene.java
+++ src/main/java/org/remote/desktop/scene/impl/RPCS3Scene.java
@@ -1,27 +1,35 @@
 package org.remote.desktop.scene.impl;
 
 import jxdotool.xDoToolUtil;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.asmus.model.EButtonAxisMapping;
 import org.asmus.model.GamepadEvent;
-import org.asmus.model.TriggerPosition;
 import org.remote.desktop.scene.BaseScene;
 
 @Slf4j
 public class RPCS3Scene extends BaseScene {
 
+    @SneakyThrows
     @Override
     public BaseScene select(GamepadEvent e) {
-        if (e.getModifiers().contains(EButtonAxisMapping.OTHER))
+        if (e.getModifiers().contains(EButtonAxisMapping.OTHER)) {
             xDoToolUtil.pressKey("ctrl+s");
+            Thread.sleep(4000);
+            xDoToolUtil.pressKey("alt+Enter");
+        }
 
         return this;
     }
 
+    @SneakyThrows
     @Override
     public BaseScene start(GamepadEvent e) {
-        if (e.getModifiers().contains(EButtonAxisMapping.OTHER))
+        if (e.getModifiers().contains(EButtonAxisMapping.OTHER)) {
             xDoToolUtil.pressKey("ctrl+r");
+            Thread.sleep(3000);
+            xDoToolUtil.pressKey("alt+Enter");
+        }
 
         return this;
     }
