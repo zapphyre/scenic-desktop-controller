5852a4d4fcffeff9e71edc0ee6a71476b6a2f8b4
Marek Manco <marek.manco@schlieger.cz>
2025-02-21 19:46:01 +0100

    remove caffeine, don't need it;

--- src/main/java/org/remote/desktop/config/CacheConfig.java
+++ src/main/java/org/remote/desktop/config/CacheConfig.java
@@ -1,30 +1,10 @@
 package org.remote.desktop.config;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
-import com.github.benmanes.caffeine.cache.RemovalListener;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @EnableCaching
 @Configuration
 public class CacheConfig {
 
-    @Bean
-    public Caffeine caffeineConfig() {
-        return Caffeine
-                .newBuilder()
-                .removalListener((RemovalListener) (o, o2, removalCause) -> {
-                    System.out.println(removalCause);
-                });
-    }
-
-    @Bean
-    public CacheManager cacheManager(Caffeine caffeine) {
-        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
-        caffeineCacheManager.setCaffeine(caffeine);
-        return caffeineCacheManager;
-    }
 }
