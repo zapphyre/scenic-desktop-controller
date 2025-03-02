522a5d57154f09268ad14ce423b4a818aca80bcc
Marek Manco <marek.manco@schlieger.cz>
2025-03-02 18:06:09 +0100

    finding inherited works;

--- src/frontend/src/components/SceneSelect.vue
+++ src/frontend/src/components/SceneSelect.vue
@@ -10,8 +10,8 @@ import {onMounted, ref, watch} from "vue";
 const scenesRef = ref<Scene[]>([]);
 const selectedSceneRef = ref<Scene>();
 const actionsRef = ref<GPadEvent[]>([]);
-const inheritedAvailableRef = ref();
-const inheritedRef = ref();
+const inheritedAvailableRef = ref<Scene[]>();
+const inheritedRef = ref<Scene>();
 
 
 const fetchScenes = async () => {
@@ -22,15 +22,18 @@ const fetchScenes = async () => {
 
 let actions = [] as GPadEvent[];
 
-watch(selectedSceneRef, (q: Scene | undefined) => {
-  console.log("inherits", q?.inherits);
-  console.log(selectedSceneRef.value);
-  actions = selectedSceneRef.value!.gpadEvents;
-  console.log("actions", actions);
-  const otherScenes = _.filter(scenesRef.value, s => s.name !== q?.name);
-  inheritedAvailableRef.value = [...otherScenes, q?.inherits];
-  inheritedRef.value = q?.inherits;
-})
+
+const chagedScene = (event: any) => {
+  inheritedAvailableRef.value = _.filter(scenesRef.value, s => s.name !== event.value?.name);
+  console.log("inherited name ", event.value.inherits?.name)
+  console.log("chagedScene", event.value);
+  console.log("chagedScene name", event.value.name);
+
+  const inheritedScene = inheritedAvailableRef.value.find(s => s.name == event.value.inherits?.name);
+  console.log("is the same name", event.value.inherits === inheritedScene);
+  console.log("inheritedScene", inheritedScene);
+  inheritedRef.value = inheritedScene;
+}
 
 watch(inheritedRef, q => {
 
@@ -48,7 +51,10 @@ onMounted(fetchScenes);
         <!--        <div class="flex items-center">-->
 
         <FloatLabel class="w-full md:w-56" variant="on">
-          <Select name="scene" v-model="selectedSceneRef" :options="scenesRef" optionLabel="name"
+          <Select name="scene" @change="chagedScene"
+                  v-model="selectedSceneRef"
+                  :options="scenesRef"
+                  optionLabel="name"
                   class="input-item"
                   placeholder="Select a scene"/>
           <label for="scene">Scene</label>
@@ -60,17 +66,15 @@ onMounted(fetchScenes);
 
       <!--    <div class="grid grid-nogutter">-->
       <div class="col-4">
-        <!--      <div class="card flex flex-wrap justify-center gap-4">-->
-        <!--        <div class="flex items-center">-->
+<!--        <SceneWideSection :all-scenes="scenesRef" :inherited="inheritedRef" />-->
         <FloatLabel class="w-full md:w-56" variant="on">
-          <Select name="inherited" checkmark v-model="inheritedRef" :options="inheritedAvailableRef"
+          <Select name="inherited__"
+                  v-model="inheritedRef"
+                  :options="inheritedAvailableRef"
                   optionLabel="name"
-                  placeholder="Select a scene"
                   class="w-full"/>
           <label for="inherited">Inherited</label>
         </FloatLabel>
-        <!--        </div>-->
-        <!--      </div>-->
       </div>
       <!--    </div>-->
     </div>
---
+++ src/frontend/src/components/SceneWideSection.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+import Select from "primevue/select";
+import FloatLabel from "primevue/floatlabel";
+import type {Scene} from "@/model/gpadOs";
+import {ref} from "vue";
+import _ from "lodash";
+
+const inheritedAvailableRef = ref<Scene[]>();
+const inheritedSceneRef = ref<Scene>();
+
+const props = defineProps<{
+  inherited: Scene | undefined;
+  allScenes: Scene[];
+}>();
+
+inheritedAvailableRef.value = _.filter(props.allScenes, s => s?.name !== props.inherited?.name);
+console.log("avail", inheritedAvailableRef.value);
+
+</script>
+
+<template>
+  <!--      <div class="card flex flex-wrap justify-center gap-4">-->
+  <!--        <div class="flex items-center">-->
+  <FloatLabel class="w-full md:w-56" variant="on">
+    <Select name="inherited" checkmark
+            v-model="inherited"
+            :options="inheritedAvailableRef"
+            optionLabel="name"
+            class="w-full"/>
+    <label for="inherited">Inherited</label>
+  </FloatLabel>
+  <!--        </div>-->
+  <!--      </div>-->
+</template>
+
+<style scoped>
+
+</style>
