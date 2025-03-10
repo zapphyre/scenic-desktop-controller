e31fbb22b8448ebca2f5105fa70bcb9440e9e60c
zapphyre <mmanco89@gmail.com>
2025-03-10 20:43:02 +0100

    cleanup dtos;

--- src/main/java/org/remote/desktop/model/dto/XdoActionDto.java
+++ src/main/java/org/remote/desktop/model/dto/XdoActionDto.java
@@ -1,17 +1,13 @@
 package org.remote.desktop.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import lombok.*;
-import lombok.extern.jackson.Jacksonized;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.Value;
 import org.remote.desktop.model.EKeyEvt;
 
-@Data
+@Value
 @Builder
-//@Jacksonized
-@NoArgsConstructor
-@AllArgsConstructor
-@JsonIdentityInfo(generator= ObjectIdGenerators.PropertyGenerator.class, property="id")
 public class XdoActionDto {
     Long id;
     EKeyEvt keyEvt;
