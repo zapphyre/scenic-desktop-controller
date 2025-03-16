e72aa6afdad0744c9a8ec5ceb2dc120ee1e1353f
Marek Manco <marek.manco@schlieger.cz>
2025-03-16 19:31:07 +0100

    fullstack backend/frontend;

---
+++ backend/src/main/java/org/remote/desktop/property/SettingsProperties.java
@@ -0,0 +1,15 @@
+package org.remote.desktop.property;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "settings")
+public class SettingsProperties {
+    private String instanceName;
+    private Boolean allowNetworkDiscovery;
+    private String baseSceneName;
+    private Boolean disconnectLocalOnRemoteConnection;
+}
--- src/main/java/org/remote/desktop/model/JmDmsInstanceName.java
+++
@@ -1,6 +0,0 @@
-package org.remote.desktop.model;
-
-import java.util.UUID;
-
-public record JmDmsInstanceName(UUID uuid) {
-}
